da4d7b6bc3358d41f7ed71f88f326ebf983f2b6b
nuttee15 <nut_nuttee@hotmail.com>
2019-09-16 10:05:25 +0700

    Initial example

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { longdo, map, LongdoMap } from './longdo-map/LongdoMap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  initMap(){
+    map.Layers.setBase(longdo.Layers.GRAY);
+  }
+
+  // make sure to use different map key: https://map.longdo.com/api
+  render() {
+    const mapKey = '5e3612dcbfa88a77bf9cc6773e5a1545'
+    return (
+      <div className="App">
+        <LongdoMap id="longdo-map" mapKey={mapKey} callback={this.initMap} />
+      </div>
+    );
+  }
 }
 
 export default App;
