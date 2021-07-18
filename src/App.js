aaca72f5d56a0279e58ec4596cfa3085c8246d6c
dogtorshepherd <40851918+dogtorshepherd@users.noreply.github.com>
2021-07-18 22:37:43 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
 
   // make sure to use different map key: https://map.longdo.com/api
   render() {
-    const mapKey = '5e3612dcbfa88a77bf9cc6773e5a1545'
+    const mapKey = '7313fc5f380ef258448202c0029934fa'
     return (
       <div className="App">
         <LongdoMap id="longdo-map" mapKey={mapKey} callback={this.initMap} />
